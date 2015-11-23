0e6de0b56e002964e3aeb4de3b11c11deb41f3ca
KeqiLi <s1408733@sms.ed.ac.uk>
2015-11-23 20:19:58 +0000

    Addition of bike class
    Initialise the basic variables and functions (including getters and
    setters) of Bike Class

---
+++ src/bikescheme/Bike.java
@@ -0,0 +1,45 @@
+package bikescheme;
+
+/**
+ * @author KAY
+ *
+ */
+public class Bike {
+private String ID;
+private boolean rentStatus;
+
+	public Bike(String bikeID) {
+		this.ID = bikeID;
+		rentStatus = false;
+	}
+	
+	public void rentBike(){
+		if (rentStatus == false) rentStatus = true;
+		else System.out.println("WTF ARE YOU DOING? WE DONT HAVE THIS IN STOCK.");
+	}
+	
+	
+	public void returnBike(){
+		if (rentStatus == true) rentStatus = false;
+		else System.out.println("WTF ARE YOU DOING? WE ALREADY HAVE IT. ARE YOU SENDING US A BOMB???");
+	}
+
+	public boolean isRentStatus() {
+		return rentStatus;
+	}
+
+	public void setRentStatus(boolean rentStatus) {
+		this.rentStatus = rentStatus;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+	
+
+
+}
